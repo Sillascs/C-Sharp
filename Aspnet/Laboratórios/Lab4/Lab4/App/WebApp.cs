0be514972baab2f4ce959be89e572269875b81ee
Sillascs <59029146+Sillascs@users.noreply.github.com>
2020-01-15 12:45:45 -0300

    Conclusão dos laboratórios 2,3,4 e 5 de Asp.Net

---
+++ Aspnet/Laboratórios/Lab4/Lab4/App/WebApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Lab4.App
+{
+    public class WebApp
+    {
+        private static string comentarioArquivo
+        {
+            get
+            {
+                return HttpContext.Current.Server.MapPath("~/comentarios.txt");
+            }
+        }
+
+        public static void ComentarioIncluir(string nome, string comentario)
+        {
+            using (var writer = new StreamWriter(comentarioArquivo, true, Encoding.UTF8))
+            {
+                writer.WriteLine("{0:dd/MM/yyyy} - {0:HH:mm:ss}", DateTime.Now);
+
+                writer.WriteLine("{0}: {1}\r\n", nome, comentario);
+
+            }        }        public static string ComentariosObter()
+        {
+            string texto = string.Empty;
+
+            if (File.Exists(comentarioArquivo))
+            {
+                using (var reader = new StreamReader(comentarioArquivo))
+                {
+                    texto = reader.ReadToEnd();                    
+                }
+            }
+            return texto;
+        }
+    }
+}
---
+++ Aspnet/Teste1/ExemploWebform.aspx
@@ -0,0 +1,19 @@
+<%@ Page Language="C#" %>
+
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+        <form runat="server">
+            <div>
+                <h1>Exemplo Web Form</h1>
+                <asp:Calentar runat="server"></asp:Calentar>
+            </div>
+        </form>
+    </body>
+
+    </html>
