984c93d53c669d1982e5fa29f890d7c7407f5c6d
Sillascs <59029146+Sillascs@users.noreply.github.com>
2020-01-16 15:04:38 -0300

    Conclusão do Projeto SitePessoal

---
+++ Aspnet/SitePessoal/SitePessoal/Controllers/ContatoController.cs
@@ -0,0 +1,44 @@
+﻿using SitePessoal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitePessoal.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Concluido = false;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(Contato contato)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(contato.Nome) ||
+                    string.IsNullOrEmpty(contato.Email) ||
+                    string.IsNullOrEmpty(contato.Mensagem))
+                {
+                    throw new ArgumentException("Favor preencher todos os campos");
+                }
+                ViewBag.Concluido = true;
+            }
+            catch(Exception ex)
+            {
+                ViewBag.ErroMsg = ex.Message;
+                ViewBag.Concluido = false;
+            }
+            
+
+            return View("Index");
+        }
+
+
+    }
+}
