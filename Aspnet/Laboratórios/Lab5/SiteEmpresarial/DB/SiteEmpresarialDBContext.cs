73625fae8a2afd1f4a38654784e41350e92fafb7
Sillascs <59029146+Sillascs@users.noreply.github.com>
2020-01-22 09:25:22 -0300

    Conclusão Laboratório 5

---
+++ Aspnet/Laboratórios/Lab5/SiteEmpresarial/DB/SiteEmpresarialDBContext.cs
@@ -0,0 +1,21 @@
+﻿using SiteEmpresarial.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SiteEmpresarial.DB
+{
+    public class SiteEmpresarialDBContext : DbContext
+    {
+        public static string conexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\di3911\Documents\Unidas2020\C-Sharp\Aspnet\Laboratórios\Lab5\SiteEmpresarial\App_Data\SiteEmpresarialDB.mdf;Integrated Security=True";
+
+        public SiteEmpresarialDBContext() : base(conexao)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
